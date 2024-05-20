3117b14e61dd4757fb5b3b6dc23d771a4abf7223
SV3N77 <danny.jiang1234@hotmail.com>
2024-05-20 10:57:44 +1000

    add auth to home page add some reviews to home page

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+export default function Reviews() {
+  return (
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img src="" alt="" />
+    </MaxWidthWrapper>
+  );
+}
